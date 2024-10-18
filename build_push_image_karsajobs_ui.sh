27e60539f607c6d3c12d1b9cda6e3207809eadb8
Daniel Sidauruk <macbook@Daniel.local>
2024-10-19 00:11:15 +0700

    add build & push script

---
+++ build_push_image_karsajobs_ui.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Membangun Docker image dari Dockerfile
+docker build -t ghcr.io/danielsidauruk/karsajobs-ui:latest .
+
+# Login ke GitHub Packages
+echo $GITHUB_TOKEN | docker login ghcr.io -u danielsidauruk --password-stdin
+
+# Mengunggah image ke GitHub Packages
+docker push ghcr.io/danielsidauruk/karsajobs-ui:latest
